ff21e64b3f255cc0db73d9e1256c6c03327273e6
Harshith Gowda N <54528303+imharshith@users.noreply.github.com>
2020-03-10 21:15:15 +0530

    Update placementdetail.py

--- pandas operation/creating dataframe/placementdetail.py
+++ pandas operation/creating dataframe/placementdetail.py
@@ -1,1 +1,7 @@
+#BIT placement details for the academic years 2014-2018
+#creating a dataframe
 
+import pandas as pd
+placement={'NO_0F_OFFERS':[567,618,588,561,555], 'ST_ELIGIBLE':[738,780,853,856,812],'ST_PLACED':[422,528,467,468,449]}
+df=pd.DataFrame(placement,index=[2014,2015,2016,2017,2018])
+print(df)
