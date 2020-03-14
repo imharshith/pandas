e1de278d94605a2d51d7f52697e151fceceddaa7
Harshith Gowda N <54528303+imharshith@users.noreply.github.com>
2020-03-14 20:19:43 +0530

    creating cse dataframe

---
+++ pandas operation/creating dataframe/csedataframe.py
@@ -0,0 +1,13 @@
+#creating CSE dataframe 
+
+import numpy as np
+import pandas as pd
+print("CSE DEPARTMENT")
+a={'CSE_OFFERS':[126,np.nan,184,164], 'CSE_ELIGIBLE':[110,109,142,142], 'CSE_PLACED':[93,100,133,124]}
+df1=pd.DataFrame(a, index=[2015,2016,2017,2018])
+print(df1)
+
+#x=df1.dropna(axis=1)  ---this drops the column with NaN values.
+#x=df1.dropna(axis=0)  ---this drops the row with NaN values.
+#x=df1.dropna()        ---this drops the row with NaN values.
+#print(x)
