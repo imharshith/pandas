5d7365b3ef6bc9c2085ed8e7ae0fd34335d09f94
Harshith Gowda N <54528303+imharshith@users.noreply.github.com>
2020-03-15 21:52:49 +0530

    Update isedataframe.py

--- pandas operation/creating dataframe/isedataframe.py
+++ pandas operation/creating dataframe/isedataframe.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+
 print("ISE DEPARTMENT")
 a={'ISE_OFFERS':[59,np.nan,42,47], 'ISE_ELIGIBLE':[43,49,39,39], 'ISE_PLACED':[43,46,35,33]}
 df3=pd.DataFrame(a,index=[2015,2016,2017,2018])
